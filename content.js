118e02ca3e8d7f44abdcf69593ce605a7d3828b7
Michael Xu <michaeljxu11@gmail.com>
2016-11-14 02:51:19 -0800

    remove extraneous methods, no longer need to send resp to background.js

--- content.js
+++ content.js
@@ -86,21 +86,14 @@ function extractTargetElement() {
   return document.body.children[0];
 }
 
-
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     console.log("runtime listener called!");
     // If the received message has the expected format...
     console.log(msg);
     if (msg.text === 'report_back') {
-        console.log("reporting back!");
-//        console.log(document.all[0].outerHTML);
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
         generateRange();
-//        sendResponse(document.all[0].outerHTML);
         var targetText = extractTargetElement();
-//        console.log(targetText);
         var res = searchAndComputeIndex(targetText.textContent);
         var tableOfContents = generateTableOfContents(res);
         embedDiv(tableOfContents);
