82c9873a50ff99696b7afafac4b38ce5b2431d41
Michael Xu <michaeljxu11@gmail.com>
2016-11-14 00:37:32 -0800

    added scroll prototype

--- content.js
+++ content.js
@@ -1,3 +1,36 @@
+function scrollGenerator(offset) {
+  return function() {
+    window.scroll(0, offset);
+  }
+}
+
+function embedDiv() {
+  var div = document.createElement( 'div' );
+
+  var tableOfContents = document.createElement( 'ol' );
+
+  for (var i = 0; i < 10 ; i++) {
+    var listElement = document.createElement( 'li' );
+    listElement.innerHTML = "really long message item: " + i;
+    listElement.onclick = scrollGenerator(i * 1000);
+    console.log(listElement);
+    tableOfContents.append(listElement);
+  }
+
+  //append all elements
+  document.body.appendChild( div );
+  div.append(tableOfContents);
+  //set attributes for div
+  div.id = 'myDivId';
+  div.style.position = 'fixed';
+  div.style.top = '0%';
+  div.style.left = '90%';
+  div.style.width = '10%';
+  div.style.height = '100%';
+  div.style.backgroundColor = 'white';
+  div.style.opacity = '0.25';
+}
+
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     console.log("runtime listener called!");
@@ -9,5 +42,6 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
         // Call the specified callback, passing
         // the web-page's DOM content as argument
         sendResponse(document.all[0].outerHTML);
+        embedDiv()
     }
 });
